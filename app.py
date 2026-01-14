be7b63a6382a67bccbe6d185eda52e1a8ec4078e
Prashant28012002 <prashantverma0095@gmail.com>
2026-01-14 12:19:03 +0530

    Enable app to run with specified host and port
    Uncommented the main block to run the app.

--- app.py
+++ app.py
@@ -15,7 +15,9 @@ print("✅ Models loaded successfully")
 
 # -----------------------------------
 # ROUTES
-# -----------------------------------
+# ----------------------if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+-------------
 
 @app.route("/")
 def home():
